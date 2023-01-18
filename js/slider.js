4bde421a67560bd904824c56aae6cfa7bc9ca1d7
Jeanne Pacard Flammini <97599809+JeannePacard@users.noreply.github.com>
2023-01-18 15:39:29 +0100

    change slider interval

--- js/slider.js
+++ js/slider.js
@@ -28,9 +28,9 @@ function showDivs(n) {
   x[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " w3-white";
 }
-const COULDOWN_DURARION = 10_000; // 10 secondes
+const SLIDER_INTERVAL_IN_MS = 10_000; // 10 secondes
 
 setInterval(function () {
   // passe au slide suivant:
   plusDivs(1);
-}, COULDOWN_DURARION);
+}, SLIDER_INTERVAL_IN_MS);
